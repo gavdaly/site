a911c0b07684dee05890ce14bd31ca454b90c09d
Gavin Daly <gavdaly@protonmail.com>
2022-08-31 17:01:41 -0400

    add prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gavdev.xyz",
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), prefetch()]
 });
