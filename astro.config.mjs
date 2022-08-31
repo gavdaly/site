c32dbaff8df8dcd41adef598df08ca1d38a51dab
Gavin Daly <gavdaly@protonmail.com>
2022-08-31 17:51:55 -0400

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gavdev.xyz",
-  integrations: [react(), sitemap(), prefetch()]
+  integrations: [react(), sitemap(), prefetch(), tailwind()],
 });
