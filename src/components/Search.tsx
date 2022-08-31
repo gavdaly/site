9b7217ac9abf8b7e75d16580798b519ee9bf5da4
Gavin Daly <gavdaly@protonmail.com>
2022-08-31 18:23:29 -0400

    Added 404 page

---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+export function Search() {
+  <>
+    <form>
+      <label htmlFor="search">search</label>
+      <input type="search" name="search" id="search" />
+      <button type="submit">search</button>
+    </form>
+    <section>results will show here</section>
+  </>;
+}
